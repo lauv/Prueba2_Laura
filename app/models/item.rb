a539b81b58fc98f8acce6c5cc9504d35ae9bcfad
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-22 12:01:04 -0300

    Sexto commit: borrado

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
+	belongs_to :category
 	belongs_to :user, optional: true
+	after_destroy :delete_category
 
 	scope :size_x, ->(equis) {where(size: equis)}
 	scope :unassigned, ->{where(user_id: nil)}
+
+	def delete_category
+		a = Item.all.where(category_id: self.category_id)
+		if a.length == 0
+			c = Category.find_by(id: self.category_id)
+			c.destroy
+		end 
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :items, dependent: :destroy
 	validates :name, presence: true
 	validates :email, uniqueness: true
 	before_save :capitalize_name
